8e4ae0f578fd0f1f4e8735c1531cd03ef392fd66
Jonas A <jonas@lhost.se>
2021-06-22 11:26:48 +0200

    first draft

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const express = require("express");
+/* const express = require("express");
 const request = require("request");
+var cookieSession = require("cookie-session");
 const cj = request.jar();
+
+const api = require("./api/getUsers.jsx");
+
+const api_2 = require("./api/getUsersWithCookies.jsx");
 
 const app = express();
 const port = 3000;
+
+var cors = require("cors");
+app.use(cors());
 
 const apiOptions = {
   server: "http://localhost:8080/",
@@ -17,6 +25,7 @@ const requestOptions = {
     "User-Agent":
       "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
     "Set-Cookie": "cookieString",
+    Cookie: "kyes",
 
     Connection: "keep-alive",
   },
@@ -27,29 +36,52 @@ const requestOptions = {
   },
 };
 
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
+
 // example: http://localhost:3000/api/users/?api-key=foo
 app.get("/users", function (req, res, next) {
-  /* res.cookie("cookieName", "1", {
-    expires: new Date(Date.now() + 900000),
-    httpOnly: true,
-  }),*/
-
-  request(requestOptions, (err, response, body) => {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      console.log(body);
-      res.send(body);
-    } else {
-      console.log(response.statusCode);
-    }
-  });
-
-  // next();
+  api.getUsers(req, res, next);
 });
 
-/* istanbul ignore next */
+app.get("/users_with_cookies", function (req, res, next) {
+  api_2.getUsersWithCookies(req, res, next);
+});
+
+
 if (!module.parent) {
   app.listen(3000);
   console.log("Express started on port 3000");
 }
+ */
+
+var cookieSession = require("cookie-session");
+var express = require("express");
+
+var app = express();
+
+var cors = require("cors");
+app.use(cors());
+
+app.set("trust proxy", 1); // trust first proxy
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+  })
+);
+
+app.get("/", function (req, res, next) {
+  // Update views
+  req.session.views = (req.session.views || 0) + 1;
+
+  // Write response
+  res.end(req.session.views + " views");
+});
+
+app.listen(3000);
